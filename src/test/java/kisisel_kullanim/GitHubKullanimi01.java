c478dcaae5197ab666e3bcb2015257ca6233ee1b
SAS <ahmetsahin@Ahmet-MacBook-Pro.local>
2023-06-29 05:58:05 +0100

    ilk degisiklik

--- src/test/java/kisisel_kullanim/GitHubKullanimi01.java
+++ src/test/java/kisisel_kullanim/GitHubKullanimi01.java
@@ -3,5 +3,6 @@ package kisisel_kullanim;
 public class GitHubKullanimi01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("That's it");
     }
 }
