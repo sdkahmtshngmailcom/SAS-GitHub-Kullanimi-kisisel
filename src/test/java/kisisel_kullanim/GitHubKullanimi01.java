016d5b914a297e22dda20c7b712830024723e542
SAS <ahmetsahin@Ahmet-MacBook-Pro.local>
2023-06-29 07:45:49 +0100

    ahmet branch 2. degisiklik

--- src/test/java/kisisel_kullanim/GitHubKullanimi01.java
+++ src/test/java/kisisel_kullanim/GitHubKullanimi01.java
@@ -4,5 +4,6 @@ public class GitHubKullanimi01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("That's it");
+        System.out.println("Ahmet branc in de ortak class ta degisiklik yaptim");
     }
 }
--- src/test/java/kisisel_kullanim/ozellik1.java
+++ src/test/java/kisisel_kullanim/ozellik1.java
@@ -3,5 +3,6 @@ package kisisel_kullanim;
 public class ozellik1 {
     public static void main(String[] args) {
         System.out.println("Ahmet branc in da calisiyorum");
+        System.out.println("Ahmet branc inde 2. degisiklik kendime ozel class ta");
     }
 }
