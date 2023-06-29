eb69baeaaccdb9bccc79369c617e0c6d4bb00f96
SAS <ahmetsahin@Ahmet-MacBook-Pro.local>
2023-06-29 07:50:17 +0100

    Main branch degisiklik

--- src/test/java/kisisel_kullanim/GitHubKullanimi01.java
+++ src/test/java/kisisel_kullanim/GitHubKullanimi01.java
@@ -4,5 +4,8 @@ public class GitHubKullanimi01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("That's it");
+        System.out.println("Main branch degisiklik 1. satir ");
+        System.out.println("Main branch degisiklik 2. satir");
+
     }
 }
